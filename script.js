8021faa0def21e6185c58d3c996d99508f2e5ab6
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-03-20 18:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+str.toUpperCase();
+	var ans = str.reverse();
+	if(str==ans){
+		return true;
+	}
+	else{
+		return false;
+	}
 	
-	return true;
+	
 }
 module.exports = palindrome
